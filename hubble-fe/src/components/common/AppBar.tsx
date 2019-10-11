c12319ee5b7ed8e1a25a6f0834d14b67fe68793b
Norn Chronos <nlfctx@outlook.com>
2019-10-11 15:05:50 +0800

    [hubble-fe] Resolve safari compatible

--- hubble-fe/src/components/common/AppBar.tsx
+++ hubble-fe/src/components/common/AppBar.tsx
@@ -1,21 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useCallback } from 'react';
 import { observer } from 'mobx-react';
+import { useLocation } from 'wouter';
 import { AppStoreContext } from '../../stores';
 
 import './AppBar.less';
 
 const AppBar: React.FC = observer(() => {
   const appStore = useContext(AppStoreContext);
+  const [_, setLocation] = useLocation();
+
+  const setRoute = useCallback(
+    (route: string) => () => {
+      setLocation(route);
+    },
+    [setLocation]
+  );
 
   return (
     <nav className="navigator">
-      <div className="navigator-logo"></div>
+      <div className="navigator-logo" onClick={setRoute('/')}></div>
       <div className="navigator-items">
-        <div className="navigator-item active">
+        <div
+          className="navigator-item active"
+          onClick={setRoute('/graph-management')}
+        >
           <span>图管理</span>
-        </div>
-        <div className="navigator-item">
-          <span>图概览</span>
         </div>
       </div>
       <div className="navigator-additions">
