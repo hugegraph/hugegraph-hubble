03125f70422264b46e4b0a058bdd58760d5dfe59
Norn Chronos <nlfctx@outlook.com>
2019-10-11 14:54:12 +0800

    [hubble-fe] Add Routes

--- hubble-fe/src/components/App.tsx
+++ hubble-fe/src/components/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Route } from 'wouter';
 import { AppBar } from './common';
-import { GraphManagement } from './graph-management';
+import { GraphManagement, DataAnalyze } from './graph-management';
 
 const App: React.FC = () => {
   return (
     <div>
       <AppBar />
-      <GraphManagement />
+      <Route
+        path="/graph-management/:id/data-analyze"
+        component={DataAnalyze}
+      />
+      <Route path="/graph-management" component={GraphManagement} />
+      <Route path="/" component={GraphManagement} />
     </div>
   );
 };
