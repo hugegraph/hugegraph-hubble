ef01bb81dee7f40e294ca5589b248e6cf87ff34c
Norn Chronos <nlfctx@outlook.com>
2020-06-02 08:31:17 +0800

    [hubble-fe] Implement version 1.3.0

---
+++ hubble-fe/src/components/graph-management/data-import/import-tasks/datamap-configs/DataMapConfigs.tsx
@@ -0,0 +1,60 @@
+import React, { useContext, useEffect } from 'react';
+import { observer } from 'mobx-react';
+import { useTranslation } from 'react-i18next';
+import { Menu } from '@baidu/one-ui';
+
+import { DataImportRootStoreContext } from '../../../../../stores';
+import FileConfigs from './FileConfigs';
+import TypeConfigs from './TypeConfigs';
+
+import './DataMapConfigs.less';
+
+const DataMapConfigs: React.FC = observer(() => {
+  const dataImportRootStore = useContext(DataImportRootStoreContext);
+  const { dataMapStore, serverDataImportStore } = dataImportRootStore;
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    // dataMapStore.setSelectedFileId(dataMapStore.fileMapInfos[0].id);
+  }, []);
+
+  return (
+    <div className="import-tasks-step-wrapper">
+      <Menu
+        mode="inline"
+        needBorder={true}
+        style={{ width: 200, height: 'calc(100vh - 194px)' }}
+        selectedKeys={[String(dataMapStore.selectedFileId)]}
+        onClick={(e: any) => {
+          // reset state from the previous file
+          dataMapStore.resetDataMaps();
+
+          // if data import starts, do not expand collpase
+          if (!serverDataImportStore.isServerStartImport) {
+            dataMapStore.switchExpand('file', true);
+          }
+
+          dataMapStore.setSelectedFileId(Number(e.key));
+          dataMapStore.setSelectedFileInfo();
+          serverDataImportStore.switchImporting(false);
+        }}
+      >
+        {dataMapStore.fileMapInfos
+          .filter(({ name }) =>
+            dataImportRootStore.successFileUploadTaskNames.includes(name)
+          )
+          .map(({ id, name }) => (
+            <Menu.Item key={id}>
+              <span>{name}</span>
+            </Menu.Item>
+          ))}
+      </Menu>
+      <div className="import-tasks-data-map-configs">
+        <FileConfigs />
+        <TypeConfigs />
+      </div>
+    </div>
+  );
+});
+
+export default DataMapConfigs;
