372f31c9ec0b2bcb519b967e8f19716164b4cbac
imbajin <imbajin@users.noreply.github.com>
2022-02-09 19:36:39 +0800

    tiny

--- hubble-dist/assembly/travis/hugegraph-server1/install-hugegraph.sh
+++ hubble-dist/assembly/travis/hugegraph-server1/install-hugegraph.sh
@@ -6,10 +6,6 @@ SERVER_CONFIG_DIR=$(dirname "$0")
 SERVER_PARENT_DIR="hugegraph-server1"
 SERVER_DIR="${SERVER_PARENT_DIR}"/hugegraph-"$1"
 
-echo $SERVER_CONFIG_DIR
-echo $SERVER_PARENT_DIR
-echo $SERVER_DIR
-
 mkdir ${SERVER_PARENT_DIR}
 tar -zxvf hugegraph-*.tar.gz -C "${SERVER_PARENT_DIR}" >/dev/null 2>&1
 
@@ -19,7 +15,6 @@ cp "${SERVER_CONFIG_DIR}"/graphs/hugegraph1.properties "${SERVER_DIR}"/conf/grap
 cp "${SERVER_CONFIG_DIR}"/graphs/hugegraph2.properties "${SERVER_DIR}"/conf/graphs/
 
 cd "${SERVER_DIR}" && pwd
-ls ./
 
 bin/init-store.sh || exit 1
 bin/start-hugegraph.sh || exit 1
