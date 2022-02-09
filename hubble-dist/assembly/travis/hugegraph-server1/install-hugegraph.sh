9072afdb471e661117151db65c521e22877d580d
imbajin <imbajin@users.noreply.github.com>
2022-02-09 17:14:46 +0800

    tiny

--- hubble-dist/assembly/travis/hugegraph-server1/install-hugegraph.sh
+++ hubble-dist/assembly/travis/hugegraph-server1/install-hugegraph.sh
@@ -3,7 +3,7 @@
 set -ev
 
 SERVER_CONFIG_DIR=$(dirname "$0")
-SERVER_PARENT_DIR="$SERVER_CONFIG_DIR"/hugegraph-server1
+SERVER_PARENT_DIR="hugegraph-server1"
 SERVER_DIR=${SERVER_PARENT_DIR}/hugegraph-*
 
 echo $SERVER_CONFIG_DIR
@@ -11,7 +11,10 @@ echo $SERVER_PARENT_DIR
 echo $SERVER_DIR
 
 mkdir ${SERVER_PARENT_DIR}
-tar -zxvf hugegraph-*.tar.gz -C ${SERVER_PARENT_DIR} >/dev/null 2>&1
+tar -zxvf hugegraph-*.tar.gz -C "${SERVER_PARENT_DIR}" >/dev/null 2>&1
+
+ls ./* && echo "###"
+ls ./*/*
 
 cp "${SERVER_CONFIG_DIR}"/gremlin-server.yaml "${SERVER_DIR}"/conf
 cp "${SERVER_CONFIG_DIR}"/rest-server.properties "${SERVER_DIR}"/conf
