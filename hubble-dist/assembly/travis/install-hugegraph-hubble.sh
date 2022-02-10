3212782b6e82c850686830ac120be39ee009ef36
coderzc <zc1217zc@126.com>
2022-02-10 13:29:34 +0800

    fix cd command

--- hubble-dist/assembly/travis/install-hugegraph-hubble.sh
+++ hubble-dist/assembly/travis/install-hugegraph-hubble.sh
@@ -9,6 +9,4 @@ mvn -e clean package -DskipTests
 cp "${TRAVIS_DIR}"/jacocoagent.jar hugegraph-hubble-*/lib
 cp "${TRAVIS_DIR}"/start-hubble.sh hugegraph-hubble-*/bin
 
-cd hugegraph-hubble-*
-
-bin/start-hubble.sh
+hugegraph-hubble-*/bin/start-hubble.sh
